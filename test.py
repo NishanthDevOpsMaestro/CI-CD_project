005d393aaaebbeccdfdb6f9f8a98f0f941c4ccb4
DevOpsMaestro Nishanth <108169504+NishanthDevOpsMaestro@users.noreply.github.com>
2023-09-19 20:07:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,4 +12,16 @@ def client():
 def test_app_is_working(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello World!" in response.data
+    assert b"Hello Buddy!" in response.data
+
+def test_home_page_content(client):
+    response = client.get('/')
+    assert b"Welcome to our App" in response.data
+
+def test_about_page_content(client):
+    response = client.get('/about')
+    assert b"About Us" in response.data
+
+def test_contact_page_content(client):
+    response = client.get('/contact')
+    assert b"Contact Us" in response.data
